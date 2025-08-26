7bb9aa7fe11105875afe48642ec98bcf4f115a93
Aaron <blaze7451@gmail.com>
2025-08-26 17:25:26 +0800

    feat: enable batch inference

--- TODO.md
+++ TODO.md
@@ -1,8 +1,9 @@
 # TODOs
-- [ ] Batch inference
-    - [ ] padding
-    - [ ] truncate
-    - [ ] batch forward
+- [✓] Batch inference
+    - [✓] padding
+    - [✓] truncate
+    - [✓] batch forward
+    - [✓] batch size argument
 - [ ] evaluation benchmark
 - [ ] 繁簡中英Readme
 - [ ] 繁簡中英model card
--- src/zhlid/model.py
+++ src/zhlid/model.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
-from typing import List, Union
+from typing import List, Union, Optional
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
 
 
 class Model:
@@ -12,35 +11,60 @@ class Model:
         **kwargs
     ):
         self.model = AutoModelForSequenceClassification.from_pretrained(model_path, **kwargs)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)    
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.config = self.model.config
     
     def get_id2label(
         self
     ):
-        id2label = self.model.config.id2label
+        id2label = self.config.id2label
         return id2label
 
     def get_label2id(
         self
     ):
-        label2id = self.model.config.label2id
+        label2id = self.config.label2id
         return label2id
 
     def predict(
         self,
-        text: Union[str, List[str]]
+        text: Union[str, List[str]],
+        batch_size: Optional[int] = 1
     ):
-        inputs = self.tokenizer(text, return_tensors="pt")
-        with torch.no_grad():
-            logits = self.model(**inputs.to("cuda"))["logits"][0]
-        scores = nn.functional.softmax(logits, dim=-1)
-        idx = torch.argmax(scores).item()
-        pred_score = scores[idx].item()
-        pred_score = round(pred_score, 10)
+        max_length = self.config.max_position_embeddings
+        device = self.model.device
         id2label = self.get_id2label()
-        pred_label = id2label[idx]
 
-        return (pred_label, pred_score)
+        if isinstance(text, str):
+            text = [text]
+
+        res = []
+
+        for i in range(0, len(text), batch_size):
+            batch_text = text[i:i + batch_size]
+            inputs = self.tokenizer(
+                batch_text, 
+                return_tensors="pt", 
+                padding='longest', 
+                truncation=True, 
+                max_length=max_length
+            )
+
+            with torch.no_grad():
+                logits = self.model(**inputs.to(device))["logits"]
+
+            scores = nn.functional.softmax(logits, dim=-1)
+            pred_scores, pred_indices = torch.max(scores, dim=-1)
+            pred_scores = pred_scores.cpu().tolist()
+            pred_indices = pred_indices.cpu().tolist()
+
+            for score, idx in zip(pred_scores, pred_indices):
+                score = round(score, 10)
+                label = id2label[idx]
+                res_dict = {"label": label, "score": score}
+                res.append(res_dict)
+
+        return res
 
 
 def load_model(model_path: str = "MusubiAI/ZHLID", **kwargs):
